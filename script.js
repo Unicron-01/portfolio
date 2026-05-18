9d45d58da8c33f4a7cd68482d905a57d5286159e
Unicron-01 <raj385039@gmail.com>
2026-05-18 17:25:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,28 @@ document.getElementById("contact-form")
   });
 
 });
+// ACTIVE NAV HIGHLIGHT ON SCROLL
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.clientHeight;
+
+    if(pageYOffset >= sectionTop){
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+
+    if(link.getAttribute("href") === "#" + current){
+      link.classList.add("active");
+    }
+  });
+});
